22a023e8191370a28ab1d0dd03209d43e108abf9
Ibrahim <iymetwally@gmail.com>
2026-02-21 23:24:16 +0200

    Alias finished

--- src/builtin/alias.rs
+++ src/builtin/alias.rs
@@ -1,9 +1,21 @@
 use crate::parsing::ParsedCommand;
 use crate::ShellState;
 
+pub fn unquote(string: &mut String) {
+    if string.len() >= 2 && string.starts_with('"') && string.ends_with('"') {
+        string.remove(0);
+        string.pop();
+    }
+}
 pub fn alias(cmd: &ParsedCommand, shell: &mut ShellState) {
-    let key = cmd.arguments[0].split('=').next().unwrap().to_string();
-    let val = cmd.arguments[0][key.len()+1..].to_string();
+    if !cmd.arguments[0].contains('='){
+        println!("{} is not a valid alias", cmd.arguments[0]);
+        return;
+    }
+    let mut key = cmd.arguments[0].split('=').next().unwrap().to_string();
+    let mut val = cmd.arguments[0][key.len()+1..].to_string();
+    unquote(&mut key);
+    unquote(&mut val);
     shell.aliases.insert(key, val);
 }
 pub fn unalias(cmd: &ParsedCommand, shell: &mut ShellState){
@@ -20,14 +32,15 @@ pub fn unalias(cmd: &ParsedCommand, shell: &mut ShellState){
 }
 pub fn get_alias(key: &str, shell: &ShellState) -> String {
     if shell.aliases.contains_key(key) {
-        let input: Vec<&str> = shell.aliases.get(key).unwrap().split_whitespace().collect();
-        if input.is_empty(){
-            return "".to_string();
+        let val = shell.aliases.get(key).unwrap().to_string();
+        if val.is_empty(){
+            return val;
         }
-        let mut command = get_alias(input[0], shell);
-        for s in &input[1..] {
-            command.push_str(" ");
-            command.push_str(s);
+        let mut command = val.split_whitespace().next().unwrap().to_string();
+        let l = command.len();
+        command = get_alias(&command, shell);
+        if l < val.len() {
+            command = [command, val[l + 1..].to_string()].concat();
         }
         return command;
     }
